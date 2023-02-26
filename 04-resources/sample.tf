ac8d4acb100e3a04f9944c5269f882a09b93095f
Hari <harishsarma.v@gmail.com>
2023-02-26 19:14:12 +0530

    updating learn-terraform

--- 04-resources/sample.tf
+++ 04-resources/sample.tf
@@ -4,8 +4,9 @@ provider "aws"{
 
 data "aws_ami" "centos8" {
   most_recent = true
-  name_regex = "Centos-8-DevOps-Practice"
-  owners = ["973714476881"]
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
 
 resource "aws_instance" "web" {
   ami             = data.aws_ami.centos8.id
