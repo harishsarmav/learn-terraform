22bd1bc6f2b39bd02780fe498d2c7dd1fcc2ba88
Hari <harishsarma.v@gmail.com>
2023-02-27 20:10:14 +0530

    updating learn-terraform

---
+++ 08-loops/count/main.tf
@@ -0,0 +1,27 @@
+resource "aws_instance" "web" {
+  count           = length(var.component)
+  ami             = data.aws_ami.centos8.id
+  instance_type   = "t3.micro"
+
+  tags = {
+    Name = "test-centos8"
+  }
+}
+
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+output "publicip" {
+  #without count
+  #value = aws_instance.web.public_ip
+
+  #with count
+  value = aws_instance.web.*.public_ip
+}
+
+variable "component" {
+  default = ["cart", "catalogue"]
+}
